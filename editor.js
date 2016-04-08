8f29861ac2d1fc49106495b0707abd651fbcdf1b
Nick Johnstone <ncwjohnstone@gmail.com>
2016-04-08 22:55:14 +1200

    Catch and display syntax errors

--- editor.js
+++ editor.js
@@ -1,5 +1,5 @@
 import {run} from '@cycle/core';
-import {makeDOMDriver, div, textarea} from '@cycle/dom';
+import {makeDOMDriver, div, pre} from '@cycle/dom';
 import {makeHTTPDriver} from '@cycle/http';
 
 import {Observable as O, ReplaySubject} from 'rx';
@@ -15,23 +15,27 @@ import 'brace/theme/monokai';
 import 'brace/keybinding/vim';
 
 function aceDriver (code$) {
-  const editor = ace.edit('editor');
-  const codeChange$ = new ReplaySubject();
+  let editor;
 
-  editor.getSession().setMode('ace/mode/javascript');
-  editor.setTheme('ace/theme/monokai');
+  code$.take(1).subscribe(code => {
+    editor = ace.edit('editor-inner');
+    editor.getSession().setMode('ace/mode/javascript');
+    editor.setTheme('ace/theme/monokai');
 
-  editor.getSession().setOptions({
-    tabSize: 2
-  });
+    editor.getSession().setOptions({
+      tabSize: 2
+    });
+
+    editor.on('input', sendCodeToSource);
+  })
+
+  const codeChange$ = new ReplaySubject(1);
 
   code$.subscribe(code => {
     editor.setValue(code);
 
     editor.clearSelection();
   });
-
-  editor.on('input', sendCodeToSource);
 
   function sendCodeToSource () {
     codeChange$.onNext({code: editor.getSession().getValue()});
@@ -58,22 +62,33 @@ function subAppDriver (code$) {
 
   let sources, sinks;
 
-  return code$.subscribe(code => {
+  const error$ = new ReplaySubject(1);
+
+  code$.subscribe(code => {
     if (sources) {
       sources.dispose();
       sinks.dispose();
     }
 
-    const subApp = execute(compile(code));
+    try {
+      const subApp = execute(compile(code));
 
-    const state = run(subApp.main, subApp.drivers);
+      const state = run(subApp.main, subApp.drivers);
 
-    sources = state.sources;
-    sinks = state.sinks;
+      sources = state.sources;
+      sinks = state.sinks;
+
+      error$.onNext({message: ''});
+    } catch (e) {
+      error$.onNext(e);
+    }
   });
+
+  return {error$: error$.startWith({message: ''})};
 }
 
 const drivers = {
+  DOM: makeDOMDriver('.tools'),
   HTTP: makeHTTPDriver({eager: true}),
   SubApp: subAppDriver,
   Ace: aceDriver
@@ -94,7 +109,7 @@ function requestApp () {
   };
 }
 
-function main ({Ace, HTTP}) {
+function main ({DOM, Ace, HTTP, SubApp}) {
   const serverCode$ = HTTP
     .filter(response$ => response$.request.method === 'GET')
     .mergeAll()
@@ -106,6 +121,13 @@ function main ({Ace, HTTP}) {
   );
 
   return {
+    DOM: SubApp.error$.map(error =>
+      div('.editor', [
+        div({key: 1, id: 'editor-inner', style: {height: error.message === '' ? '100%' : '80%'}}),
+        pre('.message-panel', {key: 2, style: {height: error.message === '' ? '0%' : '20%'}}, error.message)
+      ])
+    ),
+
     Ace: code$.pluck('code').take(1),
 
     SubApp: code$.pluck('code'),
